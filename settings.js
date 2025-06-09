bc9545372fb0327ecc9914813b0d2e01e02cd444
pathum177 <pathummalsara177@gmail.com>
2025-06-09 17:26:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {    
 SESSION_ID: process.env.SESSION_ID || 'CHAMA-MD=bc1GjJoL#29bqCq6miJTqssaZfwhKWnnUtRE-XthSr0OIcw5L9bo',
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94773024361",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94773416478",
 ALIVE: process.env.ALIVE || `default`,
-OWNER_NAME: process.env.OWNER_NAME || 'chamindu' ,     
+OWNER_NAME: process.env.OWNER_NAME || 'Pathum' ,     
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
 FOOTER: '> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀᴅʜɪ ᴏꜰᴄ',
